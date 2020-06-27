fadd848969d8d18439baac9bb70af8dbcdd00550
mdx97 <mathewhorner123@gmail.com>
2020-06-27 01:37:10 -0500

    Implemented some LD opcodes

--- boyo/CPU.cpp
+++ boyo/CPU.cpp
@@ -13,6 +13,7 @@ CPU::CPU()
 	opcode_table[0x2][0xE] = &CPU::LD_2E;
 }
 
+// Executes instructions until the given number of cycles have occurred.
 void CPU::execute(int cycles)
 {
 	int completed = 0;
@@ -20,28 +21,37 @@ void CPU::execute(int cycles)
 		completed += execute();
 }
 
+// Executes a single instruction and returns the number of cycles that it took.
 int CPU::execute()
 {
 	uint8_t opcode = bus->read(pc);
 	return (this->*opcode_table[opcode >> 4][opcode & 0xF])();
 }
 
+// Increments the PC by a given number of bytes.
 void CPU::increment_pc(int bytes = 1)
 {
 	pc += bytes;
+}
+
+// Reads the byte after the current instruction into the given register.
+int CPU::LD_imm8(int r)
+{
+	registers[r] = bus->read(pc + 8);
+	increment_pc(2);
+	return 8;
+}
+
+// Puts the value of r2 into r1.
+void CPU::LD_reg(int r1, int r2)
+{
+	registers[r1] = registers[r2];
 }
 
 int CPU::NOP()
 {
 	increment_pc();
 	return 4;
-}
-
-int CPU::LD_imm8(int reg)
-{
-	registers[reg] = bus->read(pc + 8);
-	increment_pc(2);
-	return 8;
 }
 
 int CPU::LD_06()
@@ -72,4 +82,46 @@ int CPU::LD_26()
 int CPU::LD_2E()
 {
 	return LD_imm8(7);
+}
+
+int CPU::LD_78()
+{
+	LD_reg(0, 1);
+	return 4;
+}
+
+int CPU::LD_79()
+{
+	LD_reg(0, 2);
+	return 4;
+}
+
+int CPU::LD_7A()
+{
+	LD_reg(0, 3);
+	return 4;
+}
+
+int CPU::LD_7B()
+{
+	LD_reg(0, 4);
+	return 4;
+}
+
+int CPU::LD_7C()
+{
+	LD_reg(0, 6);
+	return 4;
+}
+
+int CPU::LD_7D()
+{
+	LD_reg(0, 7);
+	return 4;
+}
+
+int CPU::LD_7F()
+{
+	LD_reg(0, 0);
+	return 4;
 }
--- boyo/CPU.h
+++ boyo/CPU.h
@@ -22,14 +22,22 @@ private:
 
 	int execute();
 	void increment_pc(int bytes);
-	int NOP();
 	int LD_imm8(int reg);
+	void LD_reg(int reg1, int reg2);
+	int NOP();
 	int LD_06();
 	int LD_0E();
 	int LD_16();
 	int LD_1E();
 	int LD_26();
 	int LD_2E();
+	int LD_78();
+	int LD_79();
+	int LD_7A();
+	int LD_7B();
+	int LD_7C();
+	int LD_7D();
+	int LD_7F();
 };
 
 #endif
